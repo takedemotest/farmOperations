4b17f092abdb94d8a03b65718c07be699e6e636b
takedemotest <takedemotest@gmail.com>
2026-08-02 15:07:16 +0530

    udpating files

---
+++ dist/farm-operations-mfe/browser/fieldsModule-XN4ML5NJ.js
@@ -0,0 +1,6 @@
+import * as e from "@angular/core";
+var o = class t {
+    static \u0275fac = function (n) { return new (n || t); };
+    static \u0275cmp = e.\u0275\u0275defineComponent({ type: t, selectors: [["app-fields-component"]], decls: 2, vars: 0, template: function (n, s) { n & 1 && (e.\u0275\u0275domElementStart(0, "p"), e.\u0275\u0275text(1, "fields-component works!"), e.\u0275\u0275domElementEnd()); }, encapsulation: 2 });
+};
+export { o as FieldsComponent };
